df32866f8058f8fffddec9f1ec87e09d11ca5acd
jerry117 <614237720@qq.com>
2017-04-06 22:00:01 +0800

    update views for close windows

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -24,6 +25,8 @@ def login_action(request):
             return response
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
+
+@login_required
 def event_manage(request):
     username = request.session.get('user', '')
     return render(request, "event_manage.html", {"user": username})
