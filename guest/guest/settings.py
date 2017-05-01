dbde3fd52ec51877867216ff1f701025e65bdb9d
jerry <2826260236@qq.com>
2017-05-01 23:47:30 +0800

    update webpage

--- guest/guest/settings.py
+++ guest/guest/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sign',
+    # 添加前端框架
     'bootstrap3',
 ]
 
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from sign.models import Event
 
 # Create your views here.
 def index(request):
@@ -28,5 +29,6 @@ def login_action(request):
 
 @login_required
 def event_manage(request):
+    event_list = Event.objects.all()
     username = request.session.get('user', '')
-    return render(request, "event_manage.html", {"user": username})
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
