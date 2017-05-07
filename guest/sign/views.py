4aebc4b9d42f81f45d816ddeb3bb3d27a1f453a7
jerry117 <614237720@qq.com>
2017-05-07 14:54:00 +0800

    update the paginator

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event, Guest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
@@ -42,9 +43,19 @@ def search_name(request):
 
 @login_required
 def guest_manage(request):
-    guest_list = Guest.objects.all()
     username = request.session.get('user', '')
-    return render(request, "guest_manage.html", {"user": username, "guests": guest_list})
+    guest_list = Guest.objects.all()
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999),deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
 
 @login_required
 def search_realname(request):
